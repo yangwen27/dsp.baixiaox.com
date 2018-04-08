aec939de4f1cdc222a106f7247ce1e41e694147f
bluelife <thebulelife@163.com>
2018-04-08 15:11:40 +0800

    ....

--- application/models/Launch_model.php
+++ application/models/Launch_model.php
@@ -82,6 +82,8 @@ class Launch_model extends ADLINKX_Model {
 	public function update($data = array(), $where = array()) {
 		$this->db->where($where);
 		$query = $this->db->update($this->table_name, $data);
+		var_dump($this->db->affected_rows());
+		var_dump($query);
 		return $query && $this->db->affected_rows() > 0 ? true : false;
 	}
 
