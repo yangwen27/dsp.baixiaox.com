950963b6f143b0464f53f733a09d88f61b9bc1c5
bluelife <thebulelife@163.com>
2018-04-08 16:02:29 +0800

    ....

--- application/models/Store_model.php
+++ application/models/Store_model.php
@@ -84,7 +84,7 @@ class Store_model extends ADLINKX_Model {
 		// var_dump($charge);
 		$achieve_payment = $this->dsatem->query_store_achieve_payment($store_id, $start_date, $end_date);
 		// var_dump($achieve_payment);
-		$roi = round(($achieve_payment / $charge), 2);
+		$roi = $charge != 0 && $achieve_payment != 0 ? round($achieve_payment / $charge, 2) : 0;
 		return $roi;
 	}
 
