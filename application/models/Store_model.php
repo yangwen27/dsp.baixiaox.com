8a239b91eae1d7dc31cab76caf5b02a8d4cbe6bb
bluelife <thebulelife@163.com>
2018-04-08 15:45:33 +0800

    ....

--- application/models/Store_model.php
+++ application/models/Store_model.php
@@ -81,9 +81,9 @@ class Store_model extends ADLINKX_Model {
 
 	public function get_roi($store_id, $start_date, $end_date) {
 		$charge = $this->dsatem->query_store_charge($store_id, $start_date, $end_date);
-		// var_dump($charge);
+		var_dump($charge);
 		$achieve_payment = $this->dsatem->query_store_achieve_payment($store_id, $start_date, $end_date);
-		// var_dump($achieve_payment);
+		var_dump($achieve_payment);
 		$roi = $charge != 0 && $achieve_payment != 0 ? round($achieve_payment / $charge, 2) : 0;
 		return $roi;
 	}
